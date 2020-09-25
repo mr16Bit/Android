e5cd5d2bb60cafdc38590b21915666fec9aaec73
mr16b1t <mr16Bit@yandex.ru>
2020-09-25 16:14:50 +0700

    Activity live cycle and change color

--- app/src/main/java/com/example/firstexemple/MainActivity.java
+++ app/src/main/java/com/example/firstexemple/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.firstexemple;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Testing_log", "onCreate method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Testing_log", "onStart method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Testing_log", "onStop method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Testing_log", "onDestroy method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Testing_log", "onPause method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Testing_log", "onResume method");
     }
 }
